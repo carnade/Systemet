49f3fa8bb3e54048c89d68dd1c9603aa7e96656a
b <lb.andreas@gmail.com>
2017-08-04 00:39:37 +0200

    Incremental changes

--- systemet.js
+++ systemet.js
@@ -1,6 +1,8 @@
 var xml2js = require('xml2js');
 var fs = require('fs');
 var mysql = require('mysql');
+var wget = require('wget-improved');
+var Prowl = require('node-prowl');
 
 fs.readFile('pwd', 'utf8', function(err, readPwd){
     var pass = readPwd.replace(/\n$/,'');
@@ -13,7 +15,34 @@ fs.readFile('pwd', 'utf8', function(err, readPwd){
     });
 //    console.log(connection);
 
-    fs.readFile('data/xml', function(err, data){
+    //wget-improved
+    var src = 'https://www.systembolaget.se/api/assortment/products/xml';
+    var today = new Date().toISOString().slice(0,10);
+    console.log('today: ' + today);
+    var output = 'data/'+ today;
+    var options = {};
+    /*var download = wget.download(src, output, options);
+    download.on('error', function(err) {
+        console.log(err);
+        process.exit(1);
+    });
+
+    //select -> delete old values
+    connection.connect(function(err) {
+      if (err) throw err;
+      console.log('before');
+          connection.query('CALL `delete_mindate_posts`', function(err, result, fields) {
+          if (err) throw err;
+          console.log('done');
+     });
+     console.log('after, before end');
+      connection.end();
+      console.log('after');
+    });
+
+    //parse and insert new values
+    fs.readFile('data/' + today , function(err, data){
+
         var parser = new xml2js.Parser();
         parser.parseString(data, function(err,result){
           //Extract the value from the data element
@@ -27,14 +56,16 @@ fs.readFile('pwd', 'utf8', function(err, readPwd){
                 //console.log('INSERT: ' + ' ' + obj.nr + ' ' + obj.Namn + ' ' + obj.Prisinklmoms);
                 post = {
                     number  : obj.nr,
+                    objectnr: obj.Varnummer,
                     name    : obj.Namn + ' ' + obj.Namn2,
                     price   : obj.Prisinklmoms,
+                    volume  : obj.Volymiml,
+                    alcohol : obj.Alkoholhalt,
                     type    : obj.Varugrupp,
                     subtype : obj.Typ,
                     origin  : obj.Ursprung,
                     country : obj.Ursprunglandnamn,
                     producer: obj.Producent,
-                    alcohol : obj.Alkoholhalt,
                     date    : createdTime
                 };
                 connection.query("INSERT INTO data SET ?", post, function (err, result, fields) {
@@ -46,10 +77,43 @@ fs.readFile('pwd', 'utf8', function(err, readPwd){
             connection.end();
           });
 
-          //console.log(result.artiklar.artikel.length);
-          console.log(createdTime);
-          //console.log(extractedData);
         });
+    });*/
+    // Select diff
+    connection.connect(function(err) {
+        if (err) throw err;
+        connection.query('CALL `get_pricereduction`', function(err, rows, fields) {
+            if (err) throw err;
+            console.log(rows);
+            console.log(rows[0].length);
+            fs.readFile('prowl', 'utf8', function(err, readProwl){
+                var prowlApiKey = readProwl.replace(/\n$/,'');
+                console.log(prowlApiKey);
+                var prowl = new Prowl(prowlApiKey);
+                if (rows[0].length < 0) {
+                    rows[0].forEach(function(result){
+                        prowl.push('Prissänkning: (' + result.object + ') ' + result.name + ' Förut: ' + result.oldPrice + ' Nu: ' + result.price + '',
+                            'Systembolaget',
+                            {priority: -2
+                             /*,url: 'http://www.systembolaget.se/sok-dryck/?searchquery=' + result.object*/},
+                            function(err, remaining) {
+                                if (err) throw err;
+                                console.log('Remaining calls: ' + remaining);
+                            });
+                    });
+                } else {
+/*                    prowl.push('För många prissänkningar: ' + rows[0].length +'st', 'Systembolaget', function(err, remaining) {
+                        if (err) throw err;
+                        console.log('Remaining calls: ' + remaining);
+                    });*/
+                }
+
+            });
+        });
+        connection.end();
     });
+    //Insert diff
+
+
 
 });
